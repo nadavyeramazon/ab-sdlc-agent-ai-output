8e9c233c6c4d7012c81a21ddf5b00d22aae762be
nadavyeramazon <nadavyer@amazon.com>
2025-11-05 20:06:11 +0200

    Initial implementation: create a hello world applicatoin in python but wit

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Hello World Application with intentional syntax error
+
+def main():
+    print("Hello, World!"
+    print("This is a simple hello world application")
+
+if __name__ == "__main__":
+    main()
