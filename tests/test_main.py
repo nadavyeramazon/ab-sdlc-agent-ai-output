991460869b641020d33054aec9ab1cf8e74c50a6
nadavyer-coder <nadavyer+coder@amazon.com>
2025-11-08 21:23:26 +0200

    Update test file to match new implementation

--- tests/test_main.py
+++ tests/test_main.py
@@ -4,6 +4,6 @@ from app.main import app
 client = TestClient(app)
 
 def test_hello_world():
-    response = client.get("/")
+    response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello, World!"}
+    assert response.json() == {'Hello': 'World'}
