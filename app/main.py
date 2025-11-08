e53a098cc5b9189a3d152ba4e306b9b8310d5d83
nadavyer-coder <nadavyer+coder@amazon.com>
2025-11-08 20:54:22 +0200

    Update FastAPI application

--- app/main.py
+++ app/main.py
@@ -2,6 +2,6 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get('/')
-def read_root():
-    return {'message': 'Hello, World!'}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
